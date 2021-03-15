9d0b248f68da2000a438c5cf3e869cfc34b215cb
MBrodo <67292707+MBrodo@users.noreply.github.com>
2021-03-16 00:38:52 +0300

    init: start project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    
+    const newBracketsConfig = bracketsConfig.map((num)=> num.join(''));
+
+    function replaceBrackets(string) {
+        const lengthBracketsConfig=newBracketsConfig.length
+        for (let i = 0; i < lengthBracketsConfig; i++) {
+            if (string.includes(newBracketsConfig[i])) {
+                string = string.replace(newBracketsConfig[i], '');
+                return replaceBrackets(string);
+            } else if (string.length === 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+    return replaceBrackets(str);
 }
